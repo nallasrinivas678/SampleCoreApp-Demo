069ee15a8a95c5b9eb59ed608c10267b81138544
Srinivas nalla <nallasrinivas678@outlook.com>
2020-09-01 00:03:16 -0400

    Added functioanlity to show list of restaurants in table. Added details page

---
+++ Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Restaurant.Core.Data;
+using Service = Restaurant.Core.Services;
+
+namespace SampleCoreApp.Pages.Restaurants
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+        public Service.Restaurant Restaurant { get; set; }
+
+        public DetailModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+        //To handle null model in razor view
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetRestaurantById(restaurantId);
+            if(Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
